c3fdf7c50c9e1dbf7b016e94a9a61db1597c7ee5
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-24 00:24:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,16 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+
+
+    erb :newteam
+  end
+
+  post '/reverse' do
+    original_string = params["string"]
+    @reversed_string = original_string.reverse
+    erb :reversed
+  end
 
 end
